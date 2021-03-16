61e1222251173339bbf3b1220910bdfbb49c3d8c
hieupt13 <hieu@dfir.vn>
2021-03-16 11:37:04 +0700

    example testing

--- pyyaxml/example.py
+++ pyyaxml/example.py
@@ -8,13 +8,10 @@ from pyyaxml.search import YaSearch
 API_USER = ''
 API_KEY = ''
 
-reload(sys)
-sys.setdefaultencoding('utf8')
-y = YaSearch(API_USER, API_KEY)
-results = y.search(u'питон', site='python.su', page=1)
-if results.error is None:
-    for result in results.items:
-        print result.url.encode('utf-8') + ' ' + result.snippet.encode('utf-8')
-else:
-    print unicode(results.error.code) + ' ' + results.error.description.encode('utf-8')
-  
+y = YaSearch(API_USER, API_KEY,'com')
+results = y.search('ricardo')
+print(results.error.description)
+for i in results.items:
+    print(i.url)
+    print(i.title)
+
